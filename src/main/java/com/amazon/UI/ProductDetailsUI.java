ac72e944d11ef72bc186fca3cae606b7aae01304
Rafael Chica <rafael.chica@sqasa.co>
2024-01-24 08:13:49 -0500

    repo de muestra

---
+++ src/main/java/com/amazon/UI/ProductDetailsUI.java
@@ -0,0 +1,11 @@
+package com.amazon.UI;
+/*
+* @(#) ProductDetailsUI.java 11.0  
+* Copyright 2023 SQA. Todos los derechos reservados.
+*/
+/**
+* @autor Rafael Chica
+* @Fecha: --o--
+*/
+public class ProductDetailsUI {
+}
